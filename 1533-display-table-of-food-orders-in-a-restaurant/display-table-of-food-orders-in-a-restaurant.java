ada230483022ef97d11eaf759cad7a5d0a4cfdfd
sanjayram <117428703+sanjayram2134@users.noreply.github.com>
2023-08-12 09:29:36 +0530

    Time: 36 ms (73.83%) | Memory: 66.5 MB (28.19%) - LeetSync

---
+++ 1533-display-table-of-food-orders-in-a-restaurant/display-table-of-food-orders-in-a-restaurant.java
@@ -0,0 +1,45 @@
+class Solution {
+    
+   
+        
+         public List<List<String>> displayTable(List<List<String>> orders) {
+        Map<Integer, Map<String, Integer>> tableInfo = new HashMap<>();
+        Set<String> foodItems = new TreeSet<>();
+        TreeSet<Integer> tableNumbers = new TreeSet<>();
+
+        // Populate the tableInfo map and tableNumbers set
+        for (List<String> order : orders) {
+            int tableNumber = Integer.parseInt(order.get(1));
+            String foodItem = order.get(2);
+
+            tableInfo.putIfAbsent(tableNumber, new HashMap<>());
+            tableInfo.get(tableNumber).put(foodItem, tableInfo.get(tableNumber).getOrDefault(foodItem, 0) + 1);
+            foodItems.add(foodItem);
+            tableNumbers.add(tableNumber);
+        }
+
+        List<List<String>> displayTable = new ArrayList<>();
+        List<String> headerRow = new ArrayList<>();
+        headerRow.add("Table");
+        headerRow.addAll(foodItems);
+        displayTable.add(headerRow);
+
+        // Create the rows for each table
+        for (int tableNumber : tableNumbers) {
+            Map<String, Integer> foodCounts = tableInfo.get(tableNumber);
+            List<String> row = new ArrayList<>();
+            row.add(Integer.toString(tableNumber));
+            for (String foodItem : foodItems) {
+                row.add(Integer.toString(foodCounts.getOrDefault(foodItem, 0)));
+            }
+            displayTable.add(row);
+        }
+
+        
+        return displayTable;
+    }
+    
+
+
+    
+}

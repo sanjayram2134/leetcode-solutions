f2385555042b13f0540091f6cb625276888526ca
sanjayram <117428703+sanjayram2134@users.noreply.github.com>
2023-12-26 20:47:32 +0530

    Time: 2 ms (89.96%) | Memory: 42.2 MB (42.65%) - LeetSync

--- 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.java
+++ 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.java
@@ -1,22 +1,22 @@
 class Solution {
     public int canBeTypedWords(String text, String brokenLetters) {
-          // Split the input text into words
+         
         String[] words = text.split(" ");
 
-        // Set to store broken letters for efficient lookup
+       
         HashSet<Character> brokenSet = new HashSet<>();
         for (char c : brokenLetters.toCharArray()) {
             brokenSet.add(c);
         }
 
-        // Variable to keep track of the count of fully typed words
+       
         int fullyTypedWords = 0;
 
-        // Iterate through each word and check for broken letters
+        
         for (String word : words) {
             boolean isWordFullyTyped = true;
 
-            // Check each letter in the word
+            
             for (char c : word.toCharArray()) {
                 if (brokenSet.contains(c)) {
                 

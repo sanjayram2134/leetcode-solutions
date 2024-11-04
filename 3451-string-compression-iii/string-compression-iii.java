eaf7b145571aba7292ba0dd3e6a0bdd0b8e66e3a
sanjayram <sanjayram.r2021ai@sece.ac.in>
2024-11-04 09:21:03 +0530

    Time: 1035 ms (8.47%) | Memory: 51.7 MB (8.75%) - LeetSync

--- 3451-string-compression-iii/string-compression-iii.java
+++ 3451-string-compression-iii/string-compression-iii.java
@@ -4,27 +4,26 @@ class Solution {
             return "";
         }
         
-        StringBuilder ans = new StringBuilder();
+        String ans = "";
         int count = 1;
         
-        // Iterate over the word starting from the second character
         for (int i = 1; i < word.length(); i++) {
             if (word.charAt(i) == word.charAt(i - 1)) {
                 count++;
                 // Limit the count to 9 for compression
                 if (count > 9) {
+                    ans += "9" + word.charAt(i - 1);
                     count = 1;
-                    ans.append(9).append(word.charAt(i - 1));
                 }
             } else {
-                ans.append(count).append(word.charAt(i - 1));
+                ans += count + "" + word.charAt(i - 1);
                 count = 1;
             }
         }
         
         // Append the last character and its count
-        ans.append(count).append(word.charAt(word.length() - 1));
+        ans += count + "" + word.charAt(word.length() - 1);
         
-        return ans.toString();
+        return ans;
     }
 }
